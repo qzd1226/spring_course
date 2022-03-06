1cc6ea749526a33187695e0a1737db2d3ba39374
qzd1226 <690160142@qq.com>
2022-03-06 14:43:03 -0500

    use spring to create obj add the test of spring in MyTest add beans.xml file(config of spring and bean of obj)

--- ch01-hello-spring/src/test/java/org/example/MyTest.java
+++ ch01-hello-spring/src/test/java/org/example/MyTest.java
@@ -3,11 +3,25 @@ package org.example;
 import org.example.service.SomeService;
 import org.example.service.impl.SomeServiceImpl;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     @Test
     public void test01(){
         SomeService service = new SomeServiceImpl();
+        service.doSome();
+    }
+    @Test
+    public void testSpring(){
+        // use spring frame work
+        // spring config
+        String config = "beans.xml";
+        //create spring object container -> ApplicationContext
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        // getBean(the id in xml file)
+        SomeService service = (SomeService) ac.getBean("someService");
+        // use the object created by spring
         service.doSome();
     }
 
