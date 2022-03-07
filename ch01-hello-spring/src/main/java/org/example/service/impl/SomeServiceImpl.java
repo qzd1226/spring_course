b53a7cb9a1020bbfbbfbefe9c23beb8b8695f7e0
qzd1226 <690160142@qq.com>
2022-03-07 11:50:57 -0500

    test when spring create object object created when we run SomeService service = (SomeService) ac.getBean("someService");

--- ch01-hello-spring/src/main/java/org/example/service/impl/SomeServiceImpl.java
+++ ch01-hello-spring/src/main/java/org/example/service/impl/SomeServiceImpl.java
@@ -3,6 +3,9 @@ package org.example.service.impl;
 import org.example.service.SomeService;
 
 public class SomeServiceImpl implements SomeService {
+    public SomeServiceImpl(){
+        System.out.println("Service construct without para");
+    }
     @Override
     public void doSome(){
         System.out.println("Run doSome() in SomeServiceImpl");
